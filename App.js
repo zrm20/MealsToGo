d06089d4c1f15c7b9e495819ac8f9c3a592b6449
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2022-12-02 13:27:37 -0600

    add rating stars

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
-import theme from './src/infrascructure/theme/index';
+import { useFonts } from "expo-font";
+import { Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { Lato_400Regular } from "@expo-google-fonts/lato";
 
+import theme from './src/infrascructure/theme/index';
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Oswald_400Regular, Lato_400Regular });
+
+  if (!fontsLoaded) {
+    return null;
+  };
+
   return (
     <>
       <ThemeProvider theme={theme}>
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from 'styled-components/native'
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+import star from '../../../../assets/star';
 
 const RestaurantCard = styled(Card)`
   background-color: ${props => props.theme.colors.bg.primary};
@@ -12,7 +15,24 @@ const RestaurantCardCover = styled(Card.Cover)`
 `;
 
 const Title = styled.Text`
+  font-family: ${props => props.theme.fonts.heading};
+  font-size: ${props => props.theme.fontSizes.heading};
+  color: ${props => props.theme.colors.ui.primary}
+`;
+
+const Info = styled.View`
   padding: ${props => props.theme.space[3]};
+`;
+
+const Address = styled.Text`
+  font-family: ${props => props.theme.fonts.body};
+  font-size: ${props => props.theme.fontSizes.caption};
+`;
+
+const RatingContainer = styled.View`
+  flex-direction: row;
+  padding-top: ${props => props.theme.space[1]};
+  padding-bottom: ${props => props.theme.space[1]};
 `;
 
 export default function RestaurantInfoCard({ restaurant = {} }) {
@@ -24,14 +44,26 @@ export default function RestaurantInfoCard({ restaurant = {} }) {
     ],
     address = "100 some random street",
     isOpenNow = true,
-    rating = 4,
+    rating = 4.2,
     isClosedTemporarilty = false,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.ceil(rating)));
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover source={{ uri: photos[0] }} key={name} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <RatingContainer>
+          {
+            ratingArray.map((el, i) => (
+              <SvgXml xml={star} width={20} height={20} key={i} />
+            ))
+          }
+        </RatingContainer>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };

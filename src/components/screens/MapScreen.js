e9e4acac6b1ba0d3bacc5e744b57beac0fcf5a92
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2022-12-04 08:53:24 -0600

    add maps

--- src/components/screens/MapScreen.js
+++ src/components/screens/MapScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, SafeAreaView } from "react-native";
+import MapView from "react-native-maps";
+import LocationSearch from "../features/LocationSearch/LocationSearch";
+
 import ScreenSafeArea from "../UI/ScreenSafeArea";
 
 export default function MapScreen(props) {
   return (
-    <ScreenSafeArea>
-      <Text>Map Screen</Text>
-    </ScreenSafeArea>
+    <MapView style={styles.map}>
+      <LocationSearch style={styles.searchBar} containerProps={{ style: styles.searchBarContainer }} />
+    </MapView>
   );
 };
 
@@ -14,6 +17,19 @@ const styles = StyleSheet.create(
   {
     container: {
 
+    },
+    map: {
+      width: '100%',
+      height: '100%'
+    },
+    searchBarContainer: {
+      width: '100%',
+      position: 'fixed',
+      zIndex: 50,
+      top: 50,
+    },
+    searchBar: {
+      position: 'relative'
     }
   }
 );
